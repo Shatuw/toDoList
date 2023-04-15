23d5f976790c8e674b2be20ea9f4644a24a806de
Shatuw <r.voigtmail@gmail.com>
2023-04-15 12:05:03 +0200

    fixed inputchatching cause false functionname

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 function funcAddNewTask() {
     // take Input (maybe proof it if its empty) an write it underneath <form>
+   
     //First: take inputfield into an variable..
+   
     let userInput = document.getElementById("newTask").value;
-    console.log('Hello' + userInput);
+    console.log("Hello test " + userInput);
+   
     //second: put it into an extra-HTML/CSS Text-Block (as variable)
+   
     //third: write the whole thing onto the page
 }
-console.log('Hello' + userInput);
+
+
 function funcMarkAsComplete (){
     //line-through the text of the task
 }
