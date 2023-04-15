39c9e4ca4d1e64f9bec29aa435a01cf218a7296c
Shatuw <r.voigtmail@gmail.com>
2023-04-15 11:17:05 +0200

    frist commit

---
+++ index.js
@@ -0,0 +1,20 @@
+function funcAddNewTask() {
+    // take Input (maybe proof it if its empty) an write it underneath <form>
+    //First: take inputfield into an variable..
+    let userInput = document.getElementById("newTask").value;
+    console.log('Hello' + userInput);
+    //second: put it into an extra-HTML/CSS Text-Block (as variable)
+    //third: write the whole thing onto the page
+}
+console.log('Hello' + userInput);
+function funcMarkAsComplete (){
+    //line-through the text of the task
+}
+
+function funcChangeTask(){
+    // change the text in the safed task
+}
+
+function funcDeleteTask(){
+    //delete the whohle task from the site
+}
