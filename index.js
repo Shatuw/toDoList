51c30ad6b272aad1fd0968075176e0cfe5debc99
kjthier <karinjthier@gmail.com>
2023-04-22 15:52:56 +0200

    merging changes

--- index.js
+++ index.js
@@ -5,46 +5,78 @@ function funcAddNewTask() {
 
     // Create a new list item and set its text content to the user's input
     let newTaskItem = document.createElement("li");
-    newTaskItem.textContent = userInput;
+    let taskText = document.createElement("span"); // create a span for the task text
+    taskText.textContent = userInput;
+    newTaskItem.appendChild(taskText);
 
     // Clear the input field
     document.getElementById("newTask").value = "";
 
     // button to mark the task as complete
     let completeButton = document.createElement("button");
-    completeButton.textContent = "Done";
+    let icon1 = document.createElement("i");
+    icon1.className = "fas fa-check"; // fa classes for the check icon
+    completeButton.appendChild(icon1);
     completeButton.onclick = function() {
-    newTaskItem.classList.add("completed");
-    taskList.appendChild(newTaskItem); // Move the completed task to the bottom of the list
+        newTaskItem.classList.add("completed");
+        taskList.appendChild(newTaskItem); // Move the completed task to the bottom of the list
     };
 
     // button to remove the task
     let removeButton = document.createElement("button");
-    removeButton.textContent = "Remove";
+    let icon2 = document.createElement("i");
+    icon2.className = "fas fa-xmark"; // fa classes for the x icon
+    removeButton.appendChild(icon2);
     removeButton.onclick = function() {
-    taskList.removeChild(newTaskItem);
+        taskList.removeChild(newTaskItem);
     };
   
     // button to edit the task
     let editButton = document.createElement("button");
-    editButton.textContent = "Edit";
+    let icon3 = document.createElement("i");
+    icon3.className = "fas fa-pencil"; // fa classes for the pencil icon
+    editButton.appendChild(icon3);
     editButton.onclick = function() {
         // Display the task text in the input field (without the text from the butttons)
-        document.getElementById("newTask").value = newTaskItem.childNodes[0].textContent;
+        document.getElementById("newTask").value = taskText.textContent;
         // Remove the task item from the list
         taskList.removeChild(newTaskItem);
     };
     
     // Add the buttons to the new task item
     newTaskItem.appendChild(completeButton);
-    newTaskItem.appendChild(removeButton);
     newTaskItem.appendChild(editButton);
+    newTaskItem.appendChild(removeButton);
+ 
+
+    // Add a class to the task text span
+    taskText.classList.add("task-text");
 
 
     // Insert the new task item at the top of the task list
-    taskList.insertBefore(newTaskItem, taskList.firstChild);
-
+    taskList.insertBefore(newTaskItem, taskList.firstChild);     
 }
+
+
+// // simulate a click event on the submit button
+// const inputField = document.getElementById("newTask");
+// const submitBtn = document.getElementById("addNewTask");
+
+// inputField.addEventListener('keydown', e) => {
+//     if (e.key === 'Enter') {
+//         e.preventDefault();
+//         submitBtn.click();
+        
+// }
+
+// empty input field doesnt add a new task
+// return completes add task function - 
+// hover over all buttons & nav links
+// daily quotes
+
+
+
+
 
 
 
